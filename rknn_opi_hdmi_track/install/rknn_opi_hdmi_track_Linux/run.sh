dc4e7f5e9984d746e927a0166b2de70e68474e8e
Lukas Strittmatter <lukas@example.com>
2026-01-29 18:52:10 +0100

    add run.sh - startet service systmd video

---
+++ rknn_opi_hdmi_track/install/rknn_opi_hdmi_track_Linux/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd /home/orangepi/repro/rknn-toolkit2/rknpu2/examples_Projektarbeit2/rknn_opi_hdmi_track/install/rknn_opi_hdmi_track_Linux/
+pwd >> log
+
+id >> log
+sudo bash run_video.sh >>log
+
+#/home/orangepi/repro/rknn-toolkit2/rknpu2/examples_Projektarbeit2/rknn_opi_hdmi_track/install/rknn_opi_hdmi_track_Linux/run.sh
